50512bcbcd54c1d1d350ab929fc2b6eac75aabce
Henrique Matheus <109422748+HenriqueMatheusDev@users.noreply.github.com>
2023-02-15 01:48:43 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 
 
-root.render(
+ReactDOM.render(
   
     <App />,
     document.getElementById('root')
